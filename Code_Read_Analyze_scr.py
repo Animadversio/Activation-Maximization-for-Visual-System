59bf67c27329643a56742f75c37f7e929d329dc3
Binxu Wang <lio50328@pku.edu.cn>
2018-09-21 16:36:44 -0500

    Add notes/comments to the Code Read Analyze scr.py

--- Code_Read_Analyze_scr.py
+++ Code_Read_Analyze_scr.py
@@ -1,15 +1,38 @@
 import utils
 import numpy as np
 import importlib
+import os
 importlib.reload(utils)  # Reload the modules after changing them, or they will not affect the codes.
 
 utils.load_codes("/home/poncelab/Documents/data/with_CNN/caffe-net_fc6_0001/backup/", 10)
 utils.load_codes_search("/home/poncelab/Documents/data/with_CNN/caffe-net_fc6_0001/backup/", "gen100")
 
-with np.load("/home/poncelab/Documents/data/with_CNN/caffe-net_fc6_0001/backup/scores_end_block298.npz") as data:
+CurDataDir = "/home/poncelab/Documents/data/with_CNN/caffe-net_fc6_0001/backup/"
+
+with np.load(CurDataDir+"backup/scores_end_block298.npz") as data:
     for key in data.keys():
         print(key)
         print(data[key])
-# ['image_ids', 'scores', 'scores_mat', 'nscores']
-# data['scores']
+# Data Structure: ['image_ids', 'scores', 'scores_mat', 'nscores']
+# 'image_ids' id, and name for files
+# data['scores'], ['scores_mat'] are nearly the same.
 # data['nscores']
+
+with np.load(CurDataDir+"backup/scores_end_block298.npz") as data:
+    for key in data.keys():
+        print(key)
+        print(data[key])
+
+with np.load(os.path.join(CurDataDir, "genealogy_gen297.npz")) as data:
+    print( data.keys() )
+    for key in data.keys():
+        print(key)
+        print(data[key])
+# ['image_ids', 'genealogy']
+# In "genealogy", They trace the 2 parents of each new stimuli in this new generation.
+# Such that a family tree can be reconstructed. And which imgs goes directly from last generation
+# and comes to this generation.
+#
+
+# os.path.join(CurDataDir, "log.txt") is all the output to the console
+
