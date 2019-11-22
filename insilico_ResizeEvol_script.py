397fb83f7e14f0c69d69eb21cb159a5b80aab3ad
Animadversio <lio50328@126.com>
2019-11-22 02:24:33 -0600

    Update insilico_ResizeEvol_script.py

--- insilico_ResizeEvol_script.py
+++ insilico_ResizeEvol_script.py
@@ -28,31 +28,33 @@ unit_arr = [#('caffe-net', 'conv5', 5, 10, 10),
             # ('caffe-net', 'fc7', 1),
             # ('caffe-net', 'fc8', 1),
             ]
-for channel in range(1, 51):
-    unit = ('caffe-net', 'conv5', channel, 10, 10)
-#for unit in unit_arr:
-    if "conv" in unit[1]:
-        rf_pos = receptive_field_for_unit(rf_dict, (3, 227, 227), layer_name_map[unit[1]], (unit[3], unit[4]))
-        imgsize = (int(rf_pos[0][1] - rf_pos[0][0]), int(rf_pos[1][1] - rf_pos[1][0]))
-        corner = (int(rf_pos[0][0]), int(rf_pos[1][0]))
-    else:
-        rf_pos = [(0, 227), (0, 227)]
-        imgsize = (227, 227)
-        corner = (0, 0)
-    exp = ExperimentResizeEvolve(unit, imgsize=imgsize, corner=corner,
-                                     max_step=100, savedir=savedir, explabel="%s_%d_rf_fit" % (unit[1], unit[2]))
-    exp.run()
-    exp.visualize_best()
-    exp.visualize_trajectory()
-    exp.visualize_exp()
-    np.savez(join(savedir, "Evolv_%s_%d_%d_%d_rf_fit.npz" % (unit[1], unit[2], unit[3], unit[4])), scores_all=exp.scores_all,
-             codes_all=exp.codes_all, generations=exp.generations)
+layer_list = ["conv4", "conv3", "conv2"]
+for layer in layer_list:
+    for channel in range(1, 51):
+        unit = ('caffe-net', layer, channel, 7, 7)
+    #for unit in unit_arr:
+        if "conv" in unit[1]:
+            rf_pos = receptive_field_for_unit(rf_dict, (3, 227, 227), layer_name_map[unit[1]], (unit[3], unit[4]))
+            imgsize = (int(rf_pos[0][1] - rf_pos[0][0]), int(rf_pos[1][1] - rf_pos[1][0]))
+            corner = (int(rf_pos[0][0]), int(rf_pos[1][0]))
+        else:
+            rf_pos = [(0, 227), (0, 227)]
+            imgsize = (227, 227)
+            corner = (0, 0)
+        exp = ExperimentResizeEvolve(unit, imgsize=imgsize, corner=corner,
+                                         max_step=100, savedir=savedir, explabel="%s_%d_rf_fit" % (unit[1], unit[2]))
+        exp.run()
+        exp.visualize_best()
+        exp.visualize_trajectory()
+        exp.visualize_exp()
+        np.savez(join(savedir, "Evolv_%s_%d_%d_%d_rf_fit.npz" % (unit[1], unit[2], unit[3], unit[4])), scores_all=exp.scores_all,
+                 codes_all=exp.codes_all, generations=exp.generations)
 
-    expo = ExperimentResizeEvolve(unit, imgsize=(227, 227), corner=(0, 0),
-                                 max_step=100, savedir=savedir, explabel="%s_%d_origin" % (unit[1], unit[2]))
-    expo.run()
-    expo.visualize_best()
-    expo.visualize_trajectory()
-    expo.visualize_exp()
-    np.savez(join(savedir, "Evolv_%s_%d_%d_%d_orig.npz" % (unit[1], unit[2], unit[3], unit[4])), scores_all=expo.scores_all,
-             codes_all=exp.codes_all, generations=expo.generations)
+        expo = ExperimentResizeEvolve(unit, imgsize=(227, 227), corner=(0, 0),
+                                     max_step=100, savedir=savedir, explabel="%s_%d_origin" % (unit[1], unit[2]))
+        expo.run()
+        expo.visualize_best()
+        expo.visualize_trajectory()
+        expo.visualize_exp()
+        np.savez(join(savedir, "Evolv_%s_%d_%d_%d_orig.npz" % (unit[1], unit[2], unit[3], unit[4])), scores_all=expo.scores_all,
+                 codes_all=exp.codes_all, generations=expo.generations)
